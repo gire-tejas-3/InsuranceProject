4c99e8b310fa28f0c2a1e185eedca0cd56a99832
gire-tejas-3 <giretejas@gmail.com>
2024-08-19 14:39:18 +0530

    First Commit

---
+++ src/test/java/com/insurance/InsuranceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insurance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
