d097602f578c41b2c6cae7c9123d8954630daba8
gire-tejas-3 <giretejas@gmail.com>
2024-08-21 18:45:38 +0530

    Exception Handler for User, Claim and Policy and Minor change in UserService

---
+++ src/main/java/com/insurance/model/Address.java
@@ -0,0 +1,90 @@
+package com.insurance.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "address")
+public class Address {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String addressLine;
+
+	private String city;
+
+	private String country;
+
+	private String pincode;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", referencedColumnName = "id")
+	private User user;
+
+	public Address() {
+		super();
+	}
+
+	public Address(String addressLine, String city, String country, String pincode) {
+		super();
+		this.addressLine = addressLine;
+		this.city = city;
+		this.country = country;
+		this.pincode = pincode;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getAddressLine() {
+		return addressLine;
+	}
+
+	public void setAddressLine(String addressLine) {
+		this.addressLine = addressLine;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getPincode() {
+		return pincode;
+	}
+
+	public void setPincode(String pincode) {
+		this.pincode = pincode;
+	}
+
+	@Override
+	public String toString() {
+		return "Address [id=" + id + ", addressLine=" + addressLine + ", city=" + city + ", country=" + country
+				+ ", pincode=" + pincode + "]";
+	}
+
+}
